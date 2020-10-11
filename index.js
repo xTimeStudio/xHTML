eb0d7768fc335afd14527d7e0e03d3c25a32fe01
xTimeStudio <69971638+xTimeStudio@users.noreply.github.com>
2020-10-11 12:46:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const version = "1.0";
 //document.getElementById('WPT').setAttribute('class', 'error')
-alert("Mady by xTime. Version: "+ version);
+//alert("Mady by xTime. Version: "+ version);
 
 // Calculator
-
+window.onload = function() {
+    var drawingCanvas = document.getElementById('smile');
+    if(drawingCanvas && drawingCanvas.getContext) {
+     var context = drawingCanvas.getContext('2d');
+     // Рисуем окружность 
+     context.strokeStyle = "#000";
+     context.fillStyle = "#fc0";
+     context.beginPath();
+     context.arc(100,100,50,0,Math.PI*2,true);
+     context.closePath();
+     context.stroke();
+     context.fill();
+     // Рисуем левый глаз 
+     context.fillStyle = "#292929";
+     context.beginPath();
+     context.arc(84,90,8,0,Math.PI*2,true);
+     context.closePath();
+     context.stroke();
+     context.fill();
+     // Рисуем правый глаз 
+     context.fillStyle = "#2fff";
+     context.beginPath();
+     context.arc(116,90,8,0,Math.PI*2,true);
+     context.closePath();
+     context.stroke();
+     context.fill();
+     // Рисуем рот
+     context.fillStyle = "#ca3939";
+     context.beginPath();
+     context.moveTo(70,115);
+     context.quadraticCurveTo(100,130,130,115);
+     context.quadraticCurveTo(100,150,70,115); 
+     context.closePath();
+     context.stroke();
+     context.fill();
+    }
+   }
 var buttonPlus = document.getElementById("buttonPlus");
 var buttonMinus = document.getElementById('buttonMinus');
 var buttonMultiply = document.getElementById('buttonMultiply');
 var buttonDevide = document.getElementById('buttonDevide');
-
+let input1 = document.getElementById('number1');
+let input2 = document.getElementById('number2');
 function onButtonClickPlus() {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
     let number1 = input1.value;
     let number2 = input2.value;
 
@@ -21,9 +55,6 @@ function onButtonClickPlus() {
     resultInput.setAttribute('value', result)
 }
 function onButtonClickMinus() {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
     let number1 = input1.value;
     let number2 = input2.value;
 
@@ -32,9 +63,6 @@ function onButtonClickMinus() {
     resultInput.setAttribute('value', result)
 }
 function onButtonClickMultiply() {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
     let number1 = input1.value;
     let number2 = input2.value;
 
@@ -43,9 +71,6 @@ function onButtonClickMultiply() {
     resultInput.setAttribute('value', result)
 }
 function onButtonClickDevide() {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
     let number1 = input1.value;
     let number2 = input2.value;
 
